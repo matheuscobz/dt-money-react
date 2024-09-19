fb70232ab8c78f98887512ab5cd6af08518b4dcd
Matheus Escobozo <40892771+matheuscobz@users.noreply.github.com>
2024-09-19 16:04:49 -0300

    Consuming local API
    Created the async function loadTransactions() to get the values from the json-server API and mapping into the tables.

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,17 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface Transaction {
+  id: number,
+  description: string,
+  type: 'income' | 'outcome',
+  price: number,
+  category: string,
+  createdAt: string
+}
+
 export function Transactions(){
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3000/transactions')
+    const data = await response.json()
+    
+    setTransactions(data)
+  }
+
   useEffect(() => {
-    fetch("http://localhost:3000/transactions")
-    .then(response => {
-        console.log(response)
-      }
-    )
+    loadTransactions()
   }, [])
 
 
@@ -25,30 +39,21 @@ export function Transactions(){
         <SearchForm />
         <TransactionsTable >
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHighlight variant="income">
-                  R$ 12.000,00
-                </PriceHighlight>
-                
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-
-            
-            
-            <tr> 
-              <td width="50%">Hamburguer</td>
-              <td>
-                <PriceHighlight variant="outcome">
-                - R$ 59,00
-                </PriceHighlight>
-              </td>
-              <td>Alimentação</td>
-              <td>10/04/2022</td>
-            </tr>
+            {transactions.map(transaction => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      R${transaction.price}
+                    </PriceHighlight>
+                    
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              )
+            })}
           </tbody>
         </TransactionsTable>
 
