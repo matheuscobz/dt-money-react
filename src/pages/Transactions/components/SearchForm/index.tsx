99f3eacde70a00b0153f122b53ca76baee8161c8
Matheus Escobozo <40892771+matheuscobz@users.noreply.github.com>
2024-10-01 15:06:04 -0300

    Created memo and finish project
    Used memo in searchForm and finished project.

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -5,14 +5,33 @@ import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { TransactionsContext } from '../../../../contexts/TransactionsContext'
 import { useContextSelector } from 'use-context-selector'
+import { memo } from 'react'
 
+/*
+  Por que um componente renderiza?
+
+  - Hooks changed (estado, contexto, reducer);
+  - Props changed (propriedades);
+  - Parent rerendered (componente pai renderizou);
+
+
+  Qual o fluxo de renderização?
+  1. O react recria o HTML da interface daquele componente
+  2. Compara a versão do HTML recriada com a versão anterior
+  3. SE mudou alguma coisa, ele reescreve o HTML na tela
+
+  Memo: 
+  0. Hooks changed, Props changed (deep comparison)
+  0.1 Comparar com a versão anterior dos hooks e props
+  0.2 SE mudou algo, ele vai permitir a nova renderização
+*/
 const searchFormSchema = z.object({
   query: z.string(),
 })
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
-export function SearchForm() {
+function SearchFormComponent() {
   const fetchTransactions = useContextSelector(
     TransactionsContext,
     (context) => {
@@ -46,3 +65,5 @@ export function SearchForm() {
     </SearchFormContainer>
   )
 }
+
+export const SearchForm = memo(SearchFormComponent)
